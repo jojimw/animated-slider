faad1124f36f834b3a2ab7ba63dcc18bb0623182
Joji Mathew <jojimathew.mec@gmail.com>
2020-04-19 21:35:11 +0530

    Updates

---
+++ precache-manifest.31184e90f6f7de9603c5b577578a4969.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb5be68746a6cafec41e6d2f138502f",
+    "url": "/animated-slider/index.html"
+  },
+  {
+    "revision": "300c2818b00e544d9de5",
+    "url": "/animated-slider/static/css/2.afd40c00.chunk.css"
+  },
+  {
+    "revision": "b6aed70fca6297517428",
+    "url": "/animated-slider/static/css/main.a9709297.chunk.css"
+  },
+  {
+    "revision": "300c2818b00e544d9de5",
+    "url": "/animated-slider/static/js/2.258a094c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/animated-slider/static/js/2.258a094c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6aed70fca6297517428",
+    "url": "/animated-slider/static/js/main.2285989d.chunk.js"
+  },
+  {
+    "revision": "de37566eda2351a29978",
+    "url": "/animated-slider/static/js/runtime-main.9d22ced4.js"
+  }
+]);
